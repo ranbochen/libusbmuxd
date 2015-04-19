62dacfd8a6e39ac92cb28b93da8c215a620b6577
Frederik Carlier <frederik.carlier@quamotion.mobi>
2015-04-19 20:18:11 +0200

    Add build number to NuGet package

--- CreateNuGetPackage.ps1
+++ CreateNuGetPackage.ps1
@@ -1,2 +1,10 @@
+Param(
+  [string]$build
+)
+
+# Update the build number
+(gc .\libusbmuxd.autoconfig).replace('{build}', $build)|sc .\libusbmuxd.out.autoconfig
+
+# Create the NuGet package
 Import-Module "C:\Program Files (x86)\Outercurve Foundation\modules\CoApp"
-Write-NuGetPackage .\libusbmuxd.autoconfig
+Write-NuGetPackage .\libusbmuxd.out.autoconfig
